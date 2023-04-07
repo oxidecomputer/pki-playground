049e85021c8c1313f70fdeacf849c65868df57f5
Rick Altherr <rick@oxidecomputer.com>
2023-04-06 17:03:33 -0700

    Allow extensions to reference earlier extensions in same cert
    Incrementally add extensions to the to-be-signed certificate so later
    extensions can reference earlier ones in the same cert.  This is
    mostly useful for Authority Key Identifier on a self-signed cert.

--- src/main.rs
+++ src/main.rs
@@ -216,26 +216,29 @@ fn main() -> Result<()> {
                     extensions: None,
                 };
 
-                let mut extensions = Vec::new();
                 for extension_config in &cert_config.extensions {
-                    extensions.push(<dyn Extension>::from_config(
+                    let extension = <dyn Extension>::from_config(
                         extension_config,
                         &tbs_cert,
                         issuer_cert.as_ref(),
-                    )?)
-                }
+                    )?;
 
-                let mut cert_extensions = Vec::new();
-                for extension in &extensions {
-                    cert_extensions.push(x509_cert::ext::Extension {
+                    let cert_extension = x509_cert::ext::Extension {
                         extn_id: extension.oid(),
                         critical: extension.is_critical(),
                         extn_value: x509_cert::der::asn1::OctetString::new(extension.as_der())
                             .into_diagnostic()?,
-                    })
+                    };
+
+                    let mut ext_vec = if let Some(x) = tbs_cert.extensions {
+                        x.clone()
+                    } else {
+                        Vec::new()
+                    };
+                    ext_vec.push(cert_extension);
+                    tbs_cert.extensions = Some(ext_vec);
                 }
 
-                tbs_cert.extensions = Some(cert_extensions);
                 let tbs_cert_der = tbs_cert.to_der().into_diagnostic()?;
 
                 let cert_signature = issuer_kp
